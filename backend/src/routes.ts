247942f2ec5d13ba2b89a4b8dab708cac189f9af
Matheus Costa <contato.matheusccp5@gmail.com>
2026-01-21 20:47:00 -0300

    feat: create route GET

---
+++ backend/src/routes.ts
@@ -0,0 +1,18 @@
+import type {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyReply,
+  FastifyRequest,
+} from "fastify";
+
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions,
+) {
+  fastify.get(
+    "/teste",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return { ok: true };
+    },
+  );
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,18 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors';
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { routes } from "./routes.js";
 
-const app = Fastify({ logger: true })
+const app = Fastify({ logger: true });
 
 const start = async () => {
+  await app.register(cors);
+  await app.register(routes);
 
-    try {
+  try {
+    await app.listen({ port: 3333 });
+  } catch (err) {
+    process.exit(1);
+  }
+};
 
-    } catch (err) {
-
-    }
-}
+start();
