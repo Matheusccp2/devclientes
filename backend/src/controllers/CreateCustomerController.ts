66b40e0a8084e1e0e67831b7a8ec6b3095094b5c
Matheus Costa <contato.matheusccp5@gmail.com>
2026-01-21 21:23:14 -0300

    feat: cadastro de cliente

---
+++ backend/src/controllers/CreateCustomerController.ts
@@ -0,0 +1,12 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { CreateCustomerService } from "../services/CreateCustomerService.js";
+
+export class CreateCustomerController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { name, email } = request.body as { name: string; email: string };
+    const customerService = new CreateCustomerService();
+    const customer = await customerService.execute({ name, email });
+
+    reply.send(customer);
+  }
+}
